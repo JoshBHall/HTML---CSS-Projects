34fc16162d51bc180a24bcb2ccd38be13a7f6c9d
JoshBHall <84826626+JoshBHall@users.noreply.github.com>
2021-08-23 14:38:09 -0400

    fixed file path

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,8 +1,3 @@
-alert("Hello, World! I am an alert box!");
-
-function My_First_Function() {
-		var String = "Kiss me, I'm Irish!";
-		var result = String.fontcolor("green");
-		document.getElementById("Irish").innerHTML =
-		result;
-	}
+window.addEventListener('DOMContentLoaded', () => {
+	document.body.classList.remove('fade-out');
+});
