dd15630a70eb0073a87034c200e2457958b7c28a
JoshBHall <84826626+JoshBHall@users.noreply.github.com>
2021-08-26 20:42:29 -0400

    moved files to appropriate repo

--- Advanced_JavaScript/form_challenge.js
+++
@@ -1,7 +0,0 @@
-function validateForm() {
-    let x = document.forms["phone#"]["phone#"].value;
-    if (x == "") {
-        alert("Phone number must be inserted");
-        return false;
-    }
-}
